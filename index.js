db25f73b5b63d7bbb3ab27897bf0d77e6e282df7
rsimp <dev@rsimp.io>
2021-01-12 10:20:58 -0600

    Fix import bug

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function classedMacro({ references, babel, state }) {
     }
 
     // replace `classed.macro` by `classed-components`
-    addDefault(program, "classsed-components", { nameHint: references.default[0].node.name });
+    addDefault(program, "classed-components", { nameHint: references.default[0].node.name });
 
     const t = babel.types;
 
